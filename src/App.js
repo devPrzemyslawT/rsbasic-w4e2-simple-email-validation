292ac3befb19c178ea9dc0af8a25bef4f3dcb206
devPrzemyslawT <przemyslaw.tomaszewski.dev@gmail.com>
2023-03-01 05:32:07 +0100

    form structure created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1> Simple e-mail validation </h1>
+			<div className='elemContainer'>
+				<label htmlFor='name'> Name: </label>
+				<input
+					id='name'
+					name='name'
+					type='textarea'
+					placeholder='Enter name...'></input>
+			</div>
+			<div className='elemContainer'>
+				<label htmlFor='email'> E-mail: </label>
+				<input
+					id='email'
+					name='email'
+					type='email'
+					placeholder='Enter e-mail...'></input>
+			</div>
+			<div className='elemContainer'>
+				<fieldset>
+					<legend>Select a blood group:</legend>
+
+					<div>
+						<label htmlFor='Group0'>Group 0</label>
+						<input
+							type='radio'
+							id='Group0'
+							name='bloodGroup'
+							value='Group 0'
+							defaultChecked></input>
+					</div>
+
+					<div>
+						<label htmlFor='GroupA'>Group A</label>
+						<input
+							type='radio'
+							id='GroupA'
+							name='bloodGroup'
+							value='Group A'></input>
+					</div>
+
+					<div>
+						<label htmlFor='GroupB'>Group B</label>
+						<input
+							type='radio'
+							id='GroupB'
+							name='bloodGroup'
+							value='Group B'></input>
+					</div>
+					<div>
+						<label htmlFor='GroupAB'>Group AB</label>
+						<input
+							type='radio'
+							id='GroupAB'
+							name='bloodGroup'
+							value='Group AB'></input>
+					</div>
+				</fieldset>
+			</div>
+
+			<div className='elemContainer'>
+				<fieldset>
+					<legend>Sex:</legend>
+
+					<div>
+						<label htmlFor='Male'>Male</label>
+						<input
+							type='radio'
+							id='Male'
+							name='Sex'
+							value='Male'
+							defaultChecked></input>
+					</div>
+
+					<div>
+						<label htmlFor='Famale'>Famale</label>
+						<input type='radio' id='Famale' name='Sex' value='Famale'></input>
+					</div>
+				</fieldset>
+			</div>
+
+			<div className='elemContainer'>
+				<input type='checkbox' id='Policy' name='Policy'></input>
+				<label htmlFor='Policy'>
+					acceptance of the regulations and privacy policy
+				</label>
+			</div>
+
+			<button type='submit'>Send</button>
+		</div>
+	);
 }
 
 export default App;
